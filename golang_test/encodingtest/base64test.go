c41ed7b20c79cefb114a18f90959d23df015fc47
john-oh <john.oh@kakaocorp.com>
2018-01-21 23:43:42 +0900

    ansible command + encoding added

---
+++ golang_test/encodingtest/base64test.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func main(){
+	input:="abcde"
+	fmt.Println(input)
+
+	beforeEncode := base64.StdEncoding.EncodeToString([]byte(input))
+	fmt.Println(beforeEncode)
+
+	afterEncode,_ := base64.StdEncoding.DecodeString(beforeEncode)
+	fmt.Println(string(afterEncode))
+}
